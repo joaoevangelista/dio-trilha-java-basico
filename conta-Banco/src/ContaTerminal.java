43cc8892359642f598b48c03765a96bc8deb0626
joaoevangelista <jesussave@gmail.com>
2024-07-18 22:07:22 -0300

    Alterado scanner para sc

--- conta-Banco/src/ContaTerminal.java
+++ conta-Banco/src/ContaTerminal.java
@@ -3,24 +3,24 @@ import java.util.Scanner;
 public class ContaTerminal {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Por favor, digite o número da conta:");
-        int numero = scanner.nextInt();
-        scanner.nextLine(); // Consumir a nova linha deixada pelo nextInt()
+        int numero = sc.nextInt();
+        sc.nextLine(); // Consumir a nova linha deixada pelo nextInt()
 
         System.out.println("Por favor, digite o número da agência:");
-        String agencia = scanner.nextLine();
+        String agencia = sc.nextLine();
 
         System.out.println("Por favor, digite o nome do cliente:");
-        String nomeCliente = scanner.nextLine();
+        String nomeCliente = sc.nextLine();
 
         System.out.println("Por favor, digite o saldo:");
-        double saldo = scanner.nextDouble();
+        double saldo = sc.nextDouble();
 
         System.out.println("Olá, " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
 
-        scanner.close();
+        sc.close();
     }
 }
 
